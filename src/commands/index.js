78d750a58ee1eabc51b122cbb34329deab132f65
Thomas Boles <tcboles@me.com>
2022-03-13 17:59:08 -0400

    Adds support for nested command directories

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,19 +1,32 @@
-import fs from 'fs';
 import path from 'path';
+import glob from 'glob';
 
 export default function add(cli) {
-  fs.readdirSync(path.resolve(__dirname)).forEach(function (file) {
-    if (file === 'index.js') {
-      return;
+  const dirpath = path.resolve(__dirname) + '/**/*';
+  glob(dirpath, (err, files) => {
+    if (err) {
+      throw err;
     }
 
-    const exportName = file
-      .replace(/\.js$/, '')
-      .split('-')
-      .map((part, index) => (index > 0 ? part[0].toUpperCase() + part.slice(1) : part))
-      .join('');
+    if (!files) {
+      throw new Error('Failed to find files at ' + dirpath);
+    }
 
-    const command = require('./' + file).default;
-    command(cli);
+    files.forEach(function (file) {
+      if (file.endsWith('index.js')) {
+        return;
+      }
+
+      if (file.endsWith('.js')) {
+        const exportName = file
+          .replace(/\.js$/, '')
+          .split('-')
+          .map((part, index) => (index > 0 ? part[0].toUpperCase() + part.slice(1) : part))
+          .join('');
+
+        const command = require(file).default;
+        command(cli);
+      }
+    });
   });
 }
